dca4404a3eec6425aac93df2323a76ec80e280b4
ipopov <ipopov4422@gmail.com>
2021-06-03 00:50:48 +0300

    Icons added, tests fixed

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,10 @@
-import React from 'react';
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import React from 'react'
+import { render, screen , waitFor} from '@testing-library/react'
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
+import App from './App'
+
+test('renders learn react link', async () => {
+  await waitFor(() => render(<App />));
+  const linkElement = screen.getByPlaceholderText("Github's username")
+  expect(linkElement).toBeInTheDocument()
+})
--- src/setupTests.ts
+++ src/setupTests.ts
@@ -1,5 +1,14 @@
-// jest-dom adds custom jest matchers for asserting on DOM nodes.
-// allows you to do things like:
-// expect(element).toHaveTextContent(/react/i)
-// learn more: https://github.com/testing-library/jest-dom
-import '@testing-library/jest-dom';
+import '@testing-library/jest-dom'
+
+// to solve problems:
+// 1. TypeError: window.matchMedia is not a function
+// 2. TypeError: Cannot read property 'addListener' of undefined
+global.matchMedia =
+  global.matchMedia ||
+  function () {
+    return {
+      matches: false,
+      addListener: jest.fn(),
+      removeListener: jest.fn(),
+    }
+  }
