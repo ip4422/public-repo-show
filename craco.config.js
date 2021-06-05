383aa9061564e703d70f9280bb274fef08999995
ipopov <ipopov4422@gmail.com>
2021-06-05 23:55:38 +0300

    Added craco, prettier rules, gitignore for package-lock.json

---
+++ craco.config.js
@@ -0,0 +1,26 @@
+const CracoLessPlugin = require('craco-less')
+
+module.exports = {
+  plugins: [
+    {
+      plugin: CracoLessPlugin,
+      options: {
+        lessLoaderOptions: {
+          lessOptions: {
+            modifyVars: {
+              '@layout-header-background': '#24292e',
+              '@breadcrumb-base-color': '#fff',
+              '@breadcrumb-last-item-color': '#fff',
+              '@breadcrumb-font-size': 'ceil(@font-size-base * 1.42)',
+              '@breadcrumb-icon-font-size': 'ceil(@font-size-base * 1.42)',
+              '@breadcrumb-link-color': '#fff',
+              '@breadcrumb-link-color-hover': 'rgba(255, 255, 255, 0.6)',
+              '@breadcrumb-separator-color': '#fff',
+            },
+            javascriptEnabled: true,
+          },
+        },
+      },
+    },
+  ],
+}
